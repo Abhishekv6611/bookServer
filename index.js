fb621e6395e5de7ad18ed8a186df86b921f68737
Abhishek v <abiabhi810@gmail.com>
2025-01-23 14:24:51 +0530

    fifth commit

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const jsonServer =require('json-server')
+const jsonServer = require('json-server');
+const MPServer = jsonServer.create();
+const middleware = jsonServer.defaults();
+const route = jsonServer.router('db.json');
+const PORT = process.env.PORT || 3000;
 
-const MPServer=jsonServer.create()
-const middleware=jsonServer.defaults()
-const PORT=3000
-const route=jsonServer.router('db.json')
+MPServer.use(middleware);
+MPServer.use(route);
 
-MPServer.use(middleware)
-MPServer.use(route)
-MPServer.listen(PORT,()=>{
-    console.log("server is running");
-    
-})
+MPServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
